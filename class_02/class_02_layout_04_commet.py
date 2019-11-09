effd7034c2e413124b26c8c6240df2946e1b4071
SoPudge <abcd666@gmail.com>
2019-11-09 11:24:54 +0800

    [ADD]class2 layout

---
+++ class_02/class_02_layout_04_commet.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QApplication
+
+class Example(QWidget):
+    """description"""
+
+    def __init__(self):
+        
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        pass
